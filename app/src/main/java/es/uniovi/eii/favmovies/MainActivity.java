6731ebb7cd94e61af1b885da3745bf1c9a27cf24
UO276213 <uo276213@uniovi.es>
2022-09-29 19:55:36 +0200

    Validar formulario al guardar

--- app/src/main/java/es/uniovi/eii/favmovies/MainActivity.java
+++ app/src/main/java/es/uniovi/eii/favmovies/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 
@@ -12,6 +13,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    String selectedCategory = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +42,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Spinner spinner = findViewById(R.id.spinnerCategory);
                 Snackbar msgCreaCategory;
-                if (spinner.getSelectedItemPosition() == 0){
+                if (spinner.getSelectedItemPosition() == 0) {
+                    selectedCategory = "";
                     msgCreaCategory = Snackbar.make(findViewById(R.id.mainLayout), R.string.create_category_msg, Snackbar.LENGTH_LONG);
-                }else {
+                } else {
+                    selectedCategory = spinner.getSelectedItem().toString();
                     msgCreaCategory = Snackbar.make(findViewById(R.id.mainLayout), R.string.modify_category, Snackbar.LENGTH_LONG);
                 }
 
@@ -59,6 +64,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private boolean validarCampos() {
+        EditText titleInput = (EditText) findViewById(R.id.titleTxtInput);
+        EditText synopsisInput = (EditText) findViewById(R.id.synopsisTxtInput);
+        EditText durationInput = (EditText) findViewById(R.id.durationTxtInput);
+        EditText dateInput = (EditText) findViewById(R.id.dateTxtInput);
+
+        if (titleInput.getText().toString().isEmpty()) {
+            Snackbar.make(findViewById(R.id.mainLayout), "Falta el título", Snackbar.LENGTH_LONG).show();
+            return false;
+        }
+        if (synopsisInput.getText().toString().isEmpty()) {
+            Snackbar.make(findViewById(R.id.mainLayout), "Falta la sinopsis", Snackbar.LENGTH_LONG).show();
+            return false;
+        }
+        if (selectedCategory.isEmpty()) {
+            Snackbar.make(findViewById(R.id.mainLayout), "Categoría sin especificar", Snackbar.LENGTH_LONG).show();
+            return false;
+        }
+        if (durationInput.getText().toString().isEmpty()) {
+            Snackbar.make(findViewById(R.id.mainLayout), "Falta la duración", Snackbar.LENGTH_LONG).show();
+            return false;
+        }
+        if (dateInput.getText().toString().isEmpty()) {
+            Snackbar.make(findViewById(R.id.mainLayout), "Falta la fecha", Snackbar.LENGTH_LONG).show();
+            return false;
+        }
+
+
         return true;
     }
 }
