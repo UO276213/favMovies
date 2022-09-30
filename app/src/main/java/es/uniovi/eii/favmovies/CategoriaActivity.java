6b985e67fe70a473814d4817679c35197b1ed79b
uo276213 <uo276213@uniovi.es>
2022-09-30 12:00:54 +0200

    Crear nueva categoría y Compartir

--- app/src/main/java/es/uniovi/eii/favmovies/CategoriaActivity.java
+++ app/src/main/java/es/uniovi/eii/favmovies/CategoriaActivity.java
@@ -2,7 +2,13 @@ package es.uniovi.eii.favmovies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import es.uniovi.eii.favmovies.modelos.Categoria;
 
 public class CategoriaActivity extends AppCompatActivity {
 
@@ -10,5 +16,46 @@ public class CategoriaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categoria);
+
+        Intent intent = getIntent();
+        int posCategoria = intent.getIntExtra(MainActivity.POS_CATEGORIA_SELEECIONADA, 0);
+
+        Categoria categoriaEntrada = null;
+
+        if (posCategoria > 0)
+            categoriaEntrada = intent.getParcelableExtra(MainActivity.CATEGORIA_SELECCIONADA);
+
+        TextView textViewCrea = findViewById(R.id.textViewCreo);
+        final EditText editNomCategoria = findViewById(R.id.editNomCategoria);
+        final EditText editDescripcion = findViewById(R.id.editDescripcion);
+
+        Button btnOk = findViewById(R.id.btnOk);
+        Button btnCancel = findViewById(R.id.btnCancel);
+
+        if (posCategoria == 0)
+            textViewCrea.setText(R.string.newCatelogryLabel);
+        else {
+            textViewCrea.setText(R.string.modify_category_existent);
+            editNomCategoria.setText(categoriaEntrada.getNombre());
+            editDescripcion.setText(categoriaEntrada.getDescripcion());
+
+            editNomCategoria.setEnabled(false);
+        }
+
+
+        btnOk.setOnClickListener(view -> {
+            Categoria categoriaSalida = new Categoria(editNomCategoria.getText().toString(), editDescripcion.getText().toString());
+            Intent intentResult = new Intent();
+            intentResult.putExtra(MainActivity.CATEGORIA_MODIFICADA, categoriaSalida);
+
+            setResult(RESULT_OK, intentResult);
+            finish();
+        });
+
+        btnCancel.setOnClickListener(view -> {
+            setResult(RESULT_CANCELED);
+            finish();
+        });
+
     }
 }
--- app/src/main/java/es/uniovi/eii/favmovies/MainActivity.java
+++ app/src/main/java/es/uniovi/eii/favmovies/MainActivity.java
@@ -1,74 +1,83 @@
 package es.uniovi.eii.favmovies;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayList;
+
+import es.uniovi.eii.favmovies.modelos.Categoria;
+import es.uniovi.eii.favmovies.modelos.Pelicula;
+import es.uniovi.eii.favmovies.util.Conexion;
+
 public class MainActivity extends AppCompatActivity {
 
     String selectedCategory = "";
+    public static final String POS_CATEGORIA_SELEECIONADA = "pos_categoria_seleccionada";
+    public static final String CATEGORIA_SELECCIONADA = "categoria_seleccionada";
+    public static final String CATEGORIA_MODIFICADA = "categoria_modificada";
+    private Spinner spinner;
 
+    public static int GESTION_CATEGORIA = 1;
+    private boolean creandoCategoria = false;
+    private ArrayList<Categoria> listaCategoria;
+    private Pelicula pelicula;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         setTitle(R.string.app_title);
 
+        listaCategoria = new ArrayList<>();
+        listaCategoria.add(new Categoria("Acción", "Película de acción"));
+        listaCategoria.add(new Categoria("Comedia", "Película de comedia"));
+
         FloatingActionButton saveBtn = findViewById(R.id.saveBtn);
+        spinner = findViewById(R.id.spinnerCategory);
+        introListaSpinner(spinner, listaCategoria);
 
         //Definimos listnener
-        saveBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Validación de cmapos
+        saveBtn.setOnClickListener(view -> {
+            // Validación de cmapos
 
-                if (validarCampos())
-                    Snackbar.make(findViewById(R.id.mainLayout), R.string.save_msg, Snackbar.LENGTH_LONG).show();
-            }
+            if (validarCampos())
+                Snackbar.make(findViewById(R.id.mainLayout), R.string.save_msg, Snackbar.LENGTH_LONG).show();
         });
 
-        Button modifyCategoryBtn = findViewById(R.id.modifyBtnCategory);
-        modifyCategoryBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setContentView(R.layout.activity_categoria);
-//                Spinner spinner = findViewById(R.id.spinnerCategory);
-//                Snackbar msgCreaCategory;
-//                if (spinner.getSelectedItemPosition() == 0) {
-//                    selectedCategory = "";
-//                    msgCreaCategory = Snackbar.make(findViewById(R.id.mainLayout), R.string.create_category_msg, Snackbar.LENGTH_LONG);
-//                } else {
-//                    selectedCategory = spinner.getSelectedItem().toString();
-//                    msgCreaCategory = Snackbar.make(findViewById(R.id.mainLayout), R.string.modify_category, Snackbar.LENGTH_LONG);
-//                }
-//
-//                // Accción de cancelar
-//                msgCreaCategory.setAction(android.R.string.cancel, new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View view) {
-//                        Snackbar.make(findViewById(R.id.mainLayout), R.string.cancel_action, Snackbar.LENGTH_LONG).show();
-//                    }
-//                });
-//
-//                msgCreaCategory.show();
-            }
+        ImageButton modifyCategoryBtn = findViewById(R.id.modifyBtnCategory);
+        modifyCategoryBtn.setOnClickListener(view -> {
+            spinner = findViewById(R.id.spinnerCategory);
+//            Snackbar msgCreaCategory;
+
+            modificarCategoria();
+
+
+            // Accción de cancelar
+//            msgCreaCategory.setAction(android.R.string.cancel, view1 -> Snackbar.make(findViewById(R.id.mainLayout), R.string.cancel_action, Snackbar.LENGTH_LONG).show());
+
+//            msgCreaCategory.show();
         });
     }
 
     private boolean validarCampos() {
-        EditText titleInput = (EditText) findViewById(R.id.titleTxtInput);
-        EditText synopsisInput = (EditText) findViewById(R.id.synopsisTxtInput);
-        EditText durationInput = (EditText) findViewById(R.id.durationTxtInput);
-        EditText dateInput = (EditText) findViewById(R.id.dateTxtInput);
+        EditText titleInput = findViewById(R.id.titleTxtInput);
+        EditText synopsisInput = findViewById(R.id.synopsisTxtInput);
+        EditText durationInput = findViewById(R.id.durationTxtInput);
+        EditText dateInput = findViewById(R.id.dateTxtInput);
 
         if (titleInput.getText().toString().isEmpty()) {
             Snackbar.make(findViewById(R.id.mainLayout), "Falta el título", Snackbar.LENGTH_LONG).show();
@@ -94,4 +103,94 @@ public class MainActivity extends AppCompatActivity {
 
         return true;
     }
+
+    private void modificarCategoria() {
+        Intent categoriaIntent = new Intent(MainActivity.this, CategoriaActivity.class);
+
+        categoriaIntent.putExtra(POS_CATEGORIA_SELEECIONADA, spinner.getSelectedItemPosition());
+        creandoCategoria = true;
+        if (spinner.getSelectedItemPosition() > 0){
+            creandoCategoria = false;
+            categoriaIntent.putExtra(CATEGORIA_SELECCIONADA, listaCategoria.get(spinner.getSelectedItemPosition() - 1));
+        }
+
+        startActivityForResult(categoriaIntent, GESTION_CATEGORIA);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GESTION_CATEGORIA){
+            if (resultCode == RESULT_OK){
+                Categoria categoriaAux = data.getParcelableExtra(CATEGORIA_MODIFICADA);
+
+                Log.d("favMovies.MainActivity", categoriaAux.toString());
+
+                if (creandoCategoria){
+                    listaCategoria.add(categoriaAux);
+                    introListaSpinner(spinner, listaCategoria);
+                } else {
+                    for (Categoria cat: listaCategoria){
+                        if (cat.getNombre().equals(categoriaAux.getNombre())) {
+                            cat.setDescripcion(categoriaAux.getDescripcion());
+                            Log.d("FavMovies.MainActivity", "Modificada la descripción de: " + cat.getNombre());
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void introListaSpinner(Spinner spinner, ArrayList<Categoria> listaCategoria) {
+        ArrayList<String> nombres = new ArrayList<>();
+
+        nombres.add("Sin definir");
+
+        for (Categoria categoria : listaCategoria){
+            nombres.add(categoria.getNombre());
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, nombres);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.Compartir){
+            Log.d("Guardar Peli", "Guardar Peli");
+//            guardarPeli();
+
+            Conexion conexion = new Conexion(getApplicationContext());
+
+            if (conexion.CompruebaConexion()){
+                compartirPeli();
+            }else
+                Toast.makeText(getApplicationContext(), R.string.compruba_conexión, Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void compartirPeli() {
+        Intent itSend = new Intent(Intent.ACTION_SEND);
+        itSend.setType("text/plain");
+
+        itSend.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.subject_compartir) + ": " + pelicula.getTitulo());
+        itSend.putExtra(Intent.EXTRA_TEXT, getString(R.string.titulo) + ": " + pelicula.getTitulo() + "\n" + getString(R.string.contenido) + ": " + pelicula.getArgumento());
+
+        Intent shareIntent = Intent.createChooser(itSend, null);
+
+        startActivity(shareIntent);
+    }
 }
+
