afaf1acea539bf34da9681e769dcc6975cf2aa88
UO276213 <uo276213@uniovi.es>
2022-10-06 20:16:47 +0200

    Avances lab3

---
+++ app/src/main/java/es/uniovi/eii/favmovies/modelos/Pelicula.java
@@ -0,0 +1,20 @@
+package es.uniovi.eii.favmovies.modelos;
+
+public class Pelicula {
+
+    private String titulo;
+    private String argumento;
+    private Categoria categoria;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getArgumento() {
+        return argumento;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+}
